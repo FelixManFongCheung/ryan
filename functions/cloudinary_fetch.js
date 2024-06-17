fa626c9d16ee89b15c48b8f2a2f408e20cc743b7
FelixManFongCheung <manfongcheung74@gmail.com>
2024-06-18 00:58:51 +0200

    test

--- functions/cloudinary_fetch.js
+++ functions/cloudinary_fetch.js
@@ -6,20 +6,21 @@ cloudinary.config({
   api_secret: process.env.REACT_APP_CLOUDINARY_SECRET,
 });
 
-exports.handler = async (event, context) => {
-  try {
-    const result = await cloudinary.search
-      .expression('tags=ryan')
-      .execute();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result.resources),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to fetch images' }),
-    };
-  }
-};
+export default async (req, context) => {
+    return new Response("Hello, world!");
+    // try {
+    //     const result = await cloudinary.search
+    //       .expression('tags=ryan')
+    //       .execute();
+    
+    //     return {
+    //       statusCode: 200,
+    //       body: JSON.stringify(result.resources),
+    //     };
+    //   } catch (error) {
+    //     return {
+    //       statusCode: 500,
+    //       body: JSON.stringify({ error: 'Failed to fetch images' }),
+    //     };
+    //   }  
+}
