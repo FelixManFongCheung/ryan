ce8201d8ea28b388ce498dca9fc292098d8faa50
FelixManFongCheung <manfongcheung74@gmail.com>
2024-06-17 17:03:49 +0200

    initial carousel framework

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
 
 export default function Works() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <div>Works</div>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item></Carousel.Item>
+    </Carousel> 
+
   )
 }
 
