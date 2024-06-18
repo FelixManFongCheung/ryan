50a9784c46f2fa5131a6f7fde961e30dc9bfd15a
FelixManFongCheung <manfongcheung74@gmail.com>
2024-06-18 12:42:00 +0200

    git

--- netlify/functions/cloudinaryFetch.js
+++ netlify/functions/cloudinaryFetch.js
@@ -6,24 +6,37 @@ cloudinary.config({
   api_secret: process.env.REACT_APP_CLOUDINARY_SECRET,
 });
 
+console.log('local');
+
 exports.handler = async function(event, context) {
-  return new Response("Hello, world!");
+  try {
+    const result = await cloudinary.search
+      .expression('tags=ryan')
+      .execute();
+
+    const optimizedImages = result.resources.map(image => ({
+      ...image,
+      secure_url: cloudinary.url(image.public_id, {
+        transformation: [
+          // { fetch_format: 'auto' }, // Automatically selects the best format
+          { quality: 'auto:best' }, // Automatically adjusts quality
+          { width: 300, crop: 'scale' }, // Example resizing
+          { dpr: 'auto' }
+        ]
+      })
+    }));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(optimizedImages),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch images' }),
+    };
+  }  
 }
 // export default async (req, context) => {
 //     return new Response("Hello, world!");
-//     // try {
-//     //     const result = await cloudinary.search
-//     //       .expression('tags=ryan')
-//     //       .execute();
-    
-//     //     return {
-//     //       statusCode: 200,
-//     //       body: JSON.stringify(result.resources),
-//     //     };
-//     //   } catch (error) {
-//     //     return {
-//     //       statusCode: 500,
-//     //       body: JSON.stringify({ error: 'Failed to fetch images' }),
-//     //     };
-//     //   }  
 // }
