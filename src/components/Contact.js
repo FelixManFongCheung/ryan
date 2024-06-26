d49e2d61eec25b81790dd3b026cb12e8f86d4ed8
FelixManFongCheung <manfongcheung74@gmail.com>
2024-06-26 16:19:37 +0200

    git

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export default function Contact() {
+  const ryanInsta = process.env.REACT_APP_RYAN_INSTAGRAM;
+  const ryanEmail = process.env.REACT_APP_RYAN_EMAIL;
+
   return (
-    <div className='content'>Contact</div>
+    <div className='content'>
+      <div className="instagram">{ryanInsta}</div>
+      <div className="email">{ryanEmail}</div>
+    </div>
   )
 }
