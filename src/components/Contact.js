2c374bbcd8dc1b95d126455de74501bb41c06983
FelixManFongCheung <manfongcheung74@gmail.com>
2024-06-30 19:00:55 +0200

    update contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 export default function Contact() {
+  const [instagram, setInstagram] = useState();
+  const [email, setEmail] = useState();
 
-  // useEffect(() => {
-  //   const fetchContact = async () => {
-  //       const response = await fetch('/.netlify/functions/cloudinaryFetch');
-  //       const data = await response.json();
-  //       console.log('render');
-  //   };
-  //   fetchContact();
-  // }, []);
+  useEffect(() => {
+    const fetchContact = async () => {
+        const response = await fetch('/.netlify/functions/cloudinaryFetch');
+        const data = await response.json();
+        setInstagram(data.instagram.default_value);
+        setEmail(data.email.default_value);
+    };
+    fetchContact();
+  }, []);
 
   return (
     <div className='content'>
-      {/* <div className="instagram">{instagram}</div>
-      <div className="email">{email}</div> */}
+      <div className="instagram">{instagram}</div>
+      <div className="email">{email}</div>
     </div>
   )
 }
