ec059b826f56ced92c0dbe7015ee805cfa6538e1
FelixManFongCheung <manfongcheung74@gmail.com>
2024-06-30 18:52:06 +0200

    styling change

--- src/components/ResponsiveImage.js
+++ src/components/ResponsiveImage.js
@@ -6,17 +6,18 @@ import { quality } from '@cloudinary/url-gen/actions/delivery';
 import { format } from '@cloudinary/url-gen/actions/delivery';
 
 
+
 const ResponsiveImage = ({ publicId }) => {
   // Create a CloudinaryImage instance
   const image = new CloudinaryImage(publicId, { cloudName: process.env.REACT_APP_CLOUDINARY_NAME });
   
-  image.resize(fill().width(500)).delivery(quality('auto:best')).delivery(format('auto'))
+  image.resize(fill().width(650)).delivery(quality('auto:best')).delivery(format('auto'))
 
   return (
     <AdvancedImage
       cldImg={image}
-      alt="Responsive Image"
-      plugins={[lazyload]}
+      plugins={[lazyload()]}
+      loading='lazy'
     />
   );
 };
