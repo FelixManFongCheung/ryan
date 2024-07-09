7b502b9db4de1d3c0ceb1df847be97f25cd789bc
FelixManFongCheung <manfongcheung74@gmail.com>
2024-07-09 18:31:27 +0200

    style change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import RouterBuilder from './App.router';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store from './redux/store.js';
 import './styles/index.scss';
 
 
